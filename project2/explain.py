308964f2817b492e7e117ff5b29341cde3424124
chrischongyj <39231238+chrischongyj@users.noreply.github.com>
2023-03-31 12:23:13 +0800

    Interface

--- project2/explain.py
+++ project2/explain.py
@@ -9,20 +9,27 @@ conn = psycopg2.connect(
 )
 
 # CHANGE HERE
-query = "SELECT * FROM customer WHERE c_acctbal < 500 AND c_nationkey = 1;"
+query1 = "SELECT * FROM customer WHERE c_acctbal < 500 AND c_nationkey = 1;"
+query2 = "SELECT * FROM customer WHERE c_nationkey = 1;"
 
 cur = conn.cursor()
 
-cur.execute(query)
-rows = cur.fetchall()
-
-for row in rows:
+cur.execute("EXPLAIN (FORMAT JSON) " + query1)
+plan1 = cur.fetchall()
+print("Plan 1:")
+for row in plan1:
     print(row)
 
-cur.execute("EXPLAIN " + query)
-plan = cur.fetchall()
-for p in plan:
-    print(p)
+cur.execute("EXPLAIN (FORMAT JSON) " + query2)
+plan2 = cur.fetchall()
+print("Plan 2:")
+for row in plan2:
+    print(row)
+
+# diff = set(plan1).difference(set(plan2))
+# print("Difference:")
+# for row in diff:
+#     print(row)
 
 cur.close()
 conn.close()
