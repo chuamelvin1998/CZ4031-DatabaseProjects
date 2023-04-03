0b716a66e352556f5817e154a17563f8159c170a
Melvin Chua <chua0946@e.ntu.edu.sg>
2023-04-03 18:04:49 +0800

    frontend done, left logic

--- project2/interface.py
+++ project2/interface.py
@@ -1,10 +1,37 @@
 import PySimpleGUI as sg
 
-# Define the layout of the interface
-layout = [[sg.Text("Query 1"), sg.InputText()],
-          [sg.Text("Query 2"), sg.InputText()],
-          [sg.Button("Submit")],
-          [sg.Multiline(size=(60, 10), key="-DIFF-")]]
+left_col = [
+    [sg.Text("Old Query")],[sg.Multiline(key='query-input1', size=(50, 5))]
+]
+
+right_col = [
+    [sg.Text("New Query")],[sg.Multiline(key='query-input2', size=(50, 5))]
+]
+
+canvasSize = 355
+
+left_col2 = [
+    [sg.Text("Old Query Visual Description")],
+    [sg.Graph(canvas_size=(canvasSize, canvasSize), graph_bottom_left=(0, 0), graph_top_right=(canvasSize, canvasSize), background_color='white', key='canvas1')],
+    [sg.Text("Old Query Text Description")],
+    [sg.Multiline(key='text-desc1', size=(50, 5),disabled=True)]
+]
+
+right_col2 = [
+    [sg.Text("New Query Visual Description")],
+    [sg.Graph(canvas_size=(canvasSize, canvasSize), graph_bottom_left=(0, 0), graph_top_right=(canvasSize, canvasSize), background_color='white', key='canvas2')],
+    [sg.Text("Old Query Text Description")],
+    [sg.Multiline(key='text-desc2', size=(50, 5),disabled=True)]
+]
+
+layout = [[sg.Column(left_col), sg.Column(right_col)],
+          [sg.Button("Compare Queries", pad=((330, 330), 10) , key='compare-button')],
+          [sg.Text("Description of Change")],
+          [sg.Multiline(key='text-desc-change', size=(107, 5), disabled=True)],
+          [sg.Column(left_col2), sg.Column(right_col2)],
+          
+          
+]
 
 # Create the window and set its title
 window = sg.Window("SQL Query Comparator", layout)
@@ -18,9 +45,9 @@ while True:
         break
     
     # If the user clicks the Submit button, compare the queries
-    if event == "Submit":
-        query1 = values[0]
-        query2 = values[1]
+    if event == "compare-button":
+        query1 = values['query-input1']
+        query2 = values['query-input2']
         
         # Compare the queries and display the differences in the -DIFF- element
         diff = ""
@@ -36,7 +63,7 @@ while True:
                     diff += f"{query1_lines[i]}\n{'-'*50}\n{query2_lines[i]}\n{'-'*50}\n"
         else:
             diff = "Queries are identical."
-        window["-DIFF-"].update(diff)
+        window["text-desc-change"].update(diff)
 
 # Close the window when the loop ends
 window.close()
